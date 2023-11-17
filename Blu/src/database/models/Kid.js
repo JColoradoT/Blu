16c69589defd31674386c4c7ae33cd40a97d8dde
pPaletas <juanpc@picxels.io>
2023-11-17 17:53:45 -0500

    kid schema added

--- Blu/src/database/models/Kid.js
+++ Blu/src/database/models/Kid.js
@@ -1,1 +1,12 @@
 const mongoose = require('mongoose');
+
+const KidSchema = new mongoose.Schema({
+    name: {
+        type: mongoose.SchemaTypes.String
+    },
+    age: {
+        type: mongoose.SchemaTypes.Number
+    }
+});
+
+module.exports = mongoose.model('kids', KidSchema);
